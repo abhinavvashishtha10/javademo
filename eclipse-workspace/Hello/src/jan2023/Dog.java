9273fc32cec4c96706caf5665b6c91d06e792a25
dipik <dipik@dipikaabhi.fritz.box>
2023-02-07 13:34:33 +0100

    this is my first

---
+++ eclipse-workspace/Hello/src/jan2023/Dog.java
@@ -0,0 +1,17 @@
+package jan2023;
+
+public class Dog {
+	String breed;
+	   int age;
+	   String color;
+
+	   void barking() {
+	   }
+	   void hungry() {
+	   }
+
+	   void sleeping() {
+	   }}
+
+
+
